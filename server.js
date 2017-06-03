5d0ce414327c9236251a990b80082572c66ee550
KortoVos <larshofschlaeger@gmail.com>
2017-06-03 21:51:57 +0200

    try

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
-var express = require('express');
-var app = express();
-var counter = 0;
+var express = require('express'),
+    app     = express(),
+    eps     = require('ejs'),
+    morgan  = require('morgan');
+
+Object.assign=require('object-assign');
+
+/*var counter = 0;
 var BALL_SPEED = 15;
 var WIDTH = 1200;
 var HEIGHT = 800;
 var TANK_INIT_HP = 100;
 var mapSize = {"w":3000,"h":3000};
-
+*/
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
 
-//Static resources server
-app.use(express.static(__dirname + '/www'));
 
 app.get('/', function (req, res) {
-
-      res.send('hello world');
-
-  
+  res.render('index.html', {});
 });
 
+app.get('/pagecount', function (req, res) {
+  res.send('{ pageCount: -1 }');
+});
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
 
 
 app.listen(port, ip);
